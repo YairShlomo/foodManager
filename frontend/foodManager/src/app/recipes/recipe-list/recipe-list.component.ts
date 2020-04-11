c7893dd42ef6ed53d67b84903c4efff5bac6eba4
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-11 19:15:41 +0300

    passing recipe data through recipe-item->recipe-list->recipes with event,data binding

--- frontend/foodManager/src/app/recipes/recipe-list/recipe-list.component.ts
+++ frontend/foodManager/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is a test','https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/07/epic-summer-salad.jpg?itok=RbLJZDhs'),
-    new Recipe('A Test Recipe','This is a test','https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/07/epic-summer-salad.jpg?itok=RbLJZDhs')
+    new Recipe('Another Test Recipe','This is a test','https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/07/epic-summer-salad.jpg?itok=RbLJZDhs')
 
   ];
   constructor() { }
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelectedRecipe(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
