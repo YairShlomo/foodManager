094751c1939472a858e6c66536e158db23aa06e5
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-11 16:53:00 +0300

    navigation with event binding,pass recipe with property binding to recipe-item

--- frontend/foodManager/src/app/header/header.component.ts
+++ frontend/foodManager/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
 
 export class HeaderComponent {    
   collapsed = true;
+  @Output() selectedFeature = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.selectedFeature.emit(feature);
+  }
+
 
 }
