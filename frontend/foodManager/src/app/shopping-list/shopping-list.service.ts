5b3ea035d8b22269baa2393a08356240f2d2baba
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-13 12:53:42 +0300

    use sl-service. notify when sl list changed

--- frontend/foodManager/src/app/shopping-list/shopping-list.service.ts
+++ frontend/foodManager/src/app/shopping-list/shopping-list.service.ts
@@ -1,5 +1,23 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Bananas',3)
+  ]
 
   constructor() { }
+
+  getIngredients() {
+    //slice for not changing the array from outside. returns copy array.
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice())
+  }
 }
