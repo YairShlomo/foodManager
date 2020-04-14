b5a78995ff77c01b032de0865c5c32e4f8dc8d1d
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-14 22:37:41 +0300

    recipe items in active style,add editing routes and retrieving route parms

--- frontend/foodManager/src/app/app-routing.module.ts
+++ frontend/foodManager/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 const routes: Routes = [
   { path: 'recipes', component: RecipesComponent, 
     children: [
+     { path: 'new', component: RecipeEditComponent },
      { path: ':id', component: RecipeDetailComponent }, 
+     { path: ':id/edit', component: RecipeEditComponent }, 
      { path: '', component: RecipeStartComponent }
 
      
