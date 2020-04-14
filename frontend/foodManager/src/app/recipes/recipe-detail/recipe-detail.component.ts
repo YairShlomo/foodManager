af0873d7307b06f6c5e6dc6de8b305e60a100b95
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-14 23:00:34 +0300

    navigate to edit and new pages

--- frontend/foodManager/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ frontend/foodManager/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -11,14 +11,14 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
   constructor(private recipeService: RecipeService,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute,
+    private router: Router ) {}
 
   ngOnInit(): void {
     this.route.params
     .subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        console.log("3")
         this.recipe = this.recipeService.getRecipe(this.id);
       }
     );
@@ -26,6 +26,11 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route });
+    //diff approach : this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route });
   }
 
 }
