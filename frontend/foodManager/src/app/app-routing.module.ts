980a6837de6251134ca74bf6a1367973495d1bbc
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-14 22:24:46 +0300

    fix page reload,child routes,configure route parms, passing dynamic parms to links

--- frontend/foodManager/src/app/app-routing.module.ts
+++ frontend/foodManager/src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 
 const routes: Routes = [
   { path: 'recipes', component: RecipesComponent, 
     children: [
-     // { path: '', component: RecipesComponent}, 
-     // { path: '', component: RecipesComponent}, 
+     { path: ':id', component: RecipeDetailComponent }, 
+     { path: '', component: RecipeStartComponent }
+
+     
+     //{ path: '', component: RecipesComponent}, 
     ] },
   { path: 'shopping-list', component: ShoppingEditComponent },
   { path: '', redirectTo: '/recipes' ,pathMatch: 'full'}
