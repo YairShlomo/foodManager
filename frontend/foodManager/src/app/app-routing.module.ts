79ae42ccc350df37ac8a4d566d18555b789559f3
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-14 19:20:18 +0300

    setting up routes,add navigation,mark active routes

--- frontend/foodManager/src/app/app-routing.module.ts
+++ frontend/foodManager/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipes', component: RecipesComponent, 
+    children: [
+     // { path: '', component: RecipesComponent}, 
+     // { path: '', component: RecipesComponent}, 
+    ] },
+  { path: 'shopping-list', component: ShoppingEditComponent },
+  { path: '', redirectTo: '/recipes' ,pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
