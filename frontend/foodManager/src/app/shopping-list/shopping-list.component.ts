cf87f50da07b4de5c0668aef1b213a083491675b
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-04-15 13:48:30 +0300

    subjects instead event-emitters

--- frontend/foodManager/src/app/shopping-list/shopping-list.component.ts
+++ frontend/foodManager/src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-
+  private ingChangedSub: Subscription;
   constructor(private sLService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.sLService.getIngredients();
-    this.sLService.ingredientsChanged.subscribe(
+    this.ingChangedSub = this.sLService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
   }
+
+  ngOnDestroy(): void {
+    this.ingChangedSub.unsubscribe();
+  }
+
 }
